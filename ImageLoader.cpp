392d4466ca432b37b327482c31f12ff4f51d7258
Muneeb Ahmad <muneeb0ahmed3@gmail.com>
2024-02-02 21:54:44 +0530

    added support for hdr images

--- ImageLoader.cpp
+++ ImageLoader.cpp
@@ -9,6 +9,17 @@
 #include "stb_image_write.h"
 #include "stb_image_resize2.h"
 
+constexpr stbir_pixel_layout pixelLayouts[4] = {
+	STBIR_1CHANNEL,
+	STBIR_2CHANNEL,
+	STBIR_RGB,
+	STBIR_RGBA
+};
+constexpr stbir_pixel_layout numComponentsToLayout(uint8_t numComponents) {
+	return pixelLayouts[numComponents - 1];
+}
+
+
 ImageData::ImageData(
 	unsigned char* data,
 	int width,
@@ -37,15 +48,8 @@ ImageData::~ImageData() {
 	STBI_FREE(this->data);
 }
 
-constexpr stbir_pixel_layout pixelLayouts[4] = {
-	STBIR_1CHANNEL,
-	STBIR_2CHANNEL,
-	STBIR_RGB,
-	STBIR_RGBA
-};
-
 std::unique_ptr<ImageData> ImageData::resize(int newWidth, int newHeight) {
-	stbir_pixel_layout pixelLayout = pixelLayouts[numComponents - 1];
+	stbir_pixel_layout pixelLayout = numComponentsToLayout(numComponents);
 	unsigned char* newImageData = (unsigned char*)STBI_MALLOC(sizeof(unsigned char) * newWidth * newHeight * numComponents);
 	if (isImageSRGB) {
 		stbir_resize_uint8_srgb(data, width, height, 0, newImageData, newWidth, newHeight, 0, pixelLayout);
@@ -99,4 +103,81 @@ size_t ImageData::getSizeInBytes() {
 
 ImagePtr loadImageFromFile(const char* filepath, bool isSRGB, int desiredComponents) {
 	return std::make_unique<ImageData>(filepath, isSRGB, desiredComponents);
+}
+
+
+FloatImageData::FloatImageData(
+	float* data,
+	int width,
+	int height,
+	int numComponents
+) : data(data), width(width), height(height), numComponents(numComponents) {}
+
+FloatImageData::FloatImageData(const char* filepath, int desiredComponents) {
+	this->data = stbi_loadf(
+		filepath,
+		&width,
+		&height,
+		&numComponents,
+		desiredComponents
+	);
+	if (desiredComponents != 0)
+		this->numComponents = desiredComponents;
+
+	if (this->data == NULL) {
+		throw std::runtime_error("Image: " + std::string(filepath) + " could not be loaded successfully");
+	}
+}
+
+FloatImageData::~FloatImageData() {
+	STBI_FREE(this->data);
+}
+
+std::unique_ptr<FloatImageData> FloatImageData::resize(int newWidth, int newHeight) {
+	stbir_pixel_layout pixelLayout = numComponentsToLayout(numComponents);
+	
+	float* newImageData = (float*)STBI_MALLOC(sizeof(unsigned char) * newWidth * newHeight * numComponents);
+	
+	stbir_resize_float_linear(data, width, height, 0, newImageData, newWidth, newHeight, 0, pixelLayout);
+	
+	FloatImageData* imageData = new FloatImageData(
+		newImageData,
+		newWidth,
+		newHeight,
+		numComponents
+	);
+
+	return FloatImagePtr(imageData);
+}
+
+float* FloatImageData::getData() {
+	return this->data;
+}
+
+int FloatImageData::getWidth() {
+	return this->width;
+}
+
+int FloatImageData::getHeight() {
+	return this->height;
+}
+
+int FloatImageData::getNumComponents() {
+	return this->numComponents;
+}
+
+glm::ivec2 FloatImageData::getResolution() {
+	return glm::ivec2(
+		this->width,
+		this->height
+	);
+}
+
+size_t FloatImageData::getSizeInBytes() {
+	assert(width > 0 && height > 0 && numComponents > 0);
+	return (size_t)this->height * this->width * this->numComponents * sizeof(float);
+}
+
+FloatImagePtr loadFloatImageFromFile(const char* filepath, int desiredComponents) {
+	return std::make_unique<FloatImageData>(filepath, desiredComponents);
 }
--- ImageLoader.hpp
+++ ImageLoader.hpp
@@ -29,6 +29,34 @@ public:
 
 	std::unique_ptr<ImageData> resize(int newWidth, int newHeight);
 };
-
 typedef std::unique_ptr<ImageData> ImagePtr;
 ImagePtr loadImageFromFile(const char* filepath, bool isSRGB, int desiredComponents);
+
+
+class FloatImageData {
+private:
+	FloatImageData(float* data, int width, int height, int numComponents);
+
+	float* data;
+	int width;
+	int height;
+	int numComponents;
+
+public:
+	FloatImageData(const char* filepath, int desiredComponents);
+	FloatImageData(const ImageData&) = delete;
+	~FloatImageData();
+
+	float* getData();
+	int getWidth();
+	int getHeight();
+	int getNumComponents();
+
+	glm::ivec2 getResolution();
+	size_t getSizeInBytes();
+
+	std::unique_ptr<FloatImageData> resize(int newWidth, int newHeight);
+};
+
+typedef std::unique_ptr<FloatImageData> FloatImagePtr;
+FloatImagePtr loadFloatImageFromFile(const char* filepath, int desiredComponents);
